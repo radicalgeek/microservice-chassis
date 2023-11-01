4c4bd03456ed9996b4dc7ff7fa45b3956112acb8
Mark Jones <mark@radicalgeek.co.uk>
2023-11-01 13:24:34 +0000

    Adding message validation and caching

--- index.js
+++ index.js
@@ -2,10 +2,14 @@ const server = require('./lib/server');
 const logger = require('./lib/logging/logger');
 const formatError = require('./lib/logging/loggerUtil');
 const mq = require('./lib/mq');
+const cacheHandler = require('./lib/cache/cacheHandler');
+const messageValidator = require('./lib/validation/messageValidator');
 
 module.exports = {
   server,
   logger,
   formatError,
-  mq
+  mq, 
+  cacheHandler,
+  messageValidator
 };
